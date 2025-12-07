eb4c2547b7f095d5370424580da1e7eb003dace1
BLE <u.ble@orange.fr>
2025-12-07 16:54:28 +0100

    update MLP

--- models/BidirectionalEmbeddingMLPClassifier.py
+++ models/BidirectionalEmbeddingMLPClassifier.py
@@ -17,8 +17,6 @@ class EmbeddingClassifier(nn.Module):
         self.score_head = nn.Sequential(
             nn.Linear(hidden_size, hidden_size),
             nn.ReLU(),
-            nn.Linear(hidden_size, hidden_size),
-            nn.ReLU(),
             nn.Linear(hidden_size, num_classes)
         )
 
